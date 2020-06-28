fc79da0ee1bd19d7ee3a504079426d9b658ebd39
Jeonghyun <yoojh9@gmail.com>
2020-06-28 19:58:21 +0900

    Finishing the Meal List Item

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../models/meal.dart';
-import 'package:flutter/foundation.dart';
 
 class MealItem extends StatelessWidget {
   final String title;
@@ -11,6 +10,40 @@ class MealItem extends StatelessWidget {
 
   MealItem(this.title, this.imageUrl, this.duration, this.complexity,
       this.affordability);
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+        break;
+      case Complexity.Challenging:
+        return 'Challenging';
+        break;
+      case Complexity.Hard:
+        return 'Hard';
+        break;
+      default:
+        return 'UnKnown';
+        break;
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+        break;
+      case Affordability.Pricey:
+        return 'Pricey';
+        break;
+      case Affordability.Luxurious:
+        return 'Luxurious';
+        break;
+      default:
+        return 'UnKnown';
+        break;
+    }
+  }
 
   void selectMeal() {}
 
@@ -32,9 +65,58 @@ class MealItem extends StatelessWidget {
                       topLeft: Radius.circular(15),
                       topRight: Radius.circular(15)),
                   child: Image.network(imageUrl,
-                      height: 250, width: double.infinity, fit: BoxFit.cover))
+                      height: 250, width: double.infinity, fit: BoxFit.cover)),
+              Positioned(
+                bottom: 20,
+                right: 10,
+                child: Container(
+                  width: 250,
+                  color: Colors.black54,
+                  padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                  child: Text(
+                    title,
+                    style: TextStyle(fontSize: 26, color: Colors.white),
+                    softWrap: true,
+                    overflow: TextOverflow.fade,
+                  ),
+                ),
+              ),
             ],
           ),
+          Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$complexityText')
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$affordabilityText')
+                    ],
+                  )
+                ],
+              )),
         ]),
       ),
     );
